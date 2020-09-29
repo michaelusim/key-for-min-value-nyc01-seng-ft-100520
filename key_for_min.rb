994a037a1812cb4adba405a81d369b591d1a63b5
michael onyeije <monyeije16@saintpeters.edu>
2020-09-29 20:37:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  arr = []
+name_hash.values.each_with_index { |i,index|
+  arr[index] = i
+}
+print name_hash.key(arr.min)
 
 end
